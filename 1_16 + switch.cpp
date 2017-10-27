71ad87f0212f0cc61ea0f7064e16129cec01d308
Kris <32760331+KrzysztofMejer@users.noreply.github.com>
2017-10-27 09:09:10 +0200

    Add files via upload

---
+++ 1_16 + switch.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   double y;
+   cout<<"Podaj x"<<endl;
+   cin>>x;
+   if (x<1) {
+    y=2*x;
+   }
+   else {
+    switch (x) {
+case 1:
+    y = -10;
+    break;
+case 3:
+    y = pow(x-1,4);
+    break;
+case 6:
+    y = sqrt((x-4));
+    break;
+default:
+y=0;
+    }
+   }
+   cout<<"Funkcja f(x) wynosi: "<<y<<endl;
+   return 0;
+}
