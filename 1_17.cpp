07fce3b76edd90e43236ed9b67dea827993210c6
Kris <32760331+KrzysztofMejer@users.noreply.github.com>
2017-10-27 09:31:09 +0200

    Add files via upload

---
+++ 1_17.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int x,y,p,q;
+    p=-5;
+    q=10;
+    srand(time(NULL));
+    x = p + rand()%(p-q+1);
+
+    if (x<7) {
+        y=pow(x,3)+1;
+    } else {
+    switch (x) {
+        case 7:
+            y=cos(x-1);
+            break;
+        case 9:
+            y=sqrt(3*x);
+            break;
+        default:
+            y = -8 * x;
+    }
+    }
+    cout<<"Y wynosi: "<<y<<endl;
+    return 0;
+    }
+
+// funkcja losuj¹ca liczby str.225 w pozdr
+// zad.dom - zad 1_18 + specyfikacja i schemat blokowy w zeszcyie do zadañ 1_14,1_15,1_16,1_17
+// za tydzieñ kartkówka z kodu
+
